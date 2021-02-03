e34f86774d31fd14effa05ba39fdffa2bf8bc888
olmoshbtn <2120@holbertonschool.com>
2021-02-03 11:43:02 +0000

    101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -27,8 +27,7 @@ void swap_node(listint_t **list, listint_t *node)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	/* Boolean values, true equals 1, false equals 0 */
-	char swapped = 1;
+	static int swapped = 1;
 	listint_t *tmp = *list;
 
 	if (list == NULL || *list == NULL)
