524073f097a73672e5ebf68c4b9f322a0377a7b7
olmoshbtn <2120@holbertonschool.com>
2021-02-03 11:35:00 +0000

    101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,11 +1,11 @@
 #include "sort.h"
 
 /**
- * swap_node - swaps a node with the next node in the list
+ * swap - swaps a node with the next node in the list
  * @list: double pointer to the head of a doubly linked list
  * @node: pointer to the node to swap
  */
-void swap_node(listint_t **list, listint_t *node)
+void swap(listint_t **list, listint_t *node)
 {
 	node->next->prev = node->prev;
 	if (node->prev)
@@ -27,40 +27,39 @@ void swap_node(listint_t **list, listint_t *node)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	/* Boolean values, true equals 1, false equals 0 */
-	static int swapped = 1;
-	listint_t *tmp = *list;
+	char swapped = 1;
+	listint_t *temp;
 
 	if (list == NULL || *list == NULL)
 		return;
-
+	temp = *list;
 	while (swapped != 0)
 	{
 		swapped = 0;
-		while (tmp->next != NULL)
+		while (temp->next != NULL)
 		{
-			if (tmp->next->n < tmp->n)
+			if (temp->next->n < temp->n)
 			{
-				swap_node(list, tmp);
+				swap(list, temp);
 				swapped = 1;
 				print_list(*list);
 			}
 			else
-			tmp = tmp->next;
+				temp = temp->next;
 		}
 		if (swapped == 0)
 			break;
 		swapped = 0;
-		while (tmp->prev != NULL)
+		while (temp->prev != NULL)
 		{
-			if (tmp->prev->n > tmp->n)
+			if (temp->prev->n > temp->n)
 			{
-				swap_node(list, tmp->prev);
+				swap(list, temp->prev);
 				swapped = 1;
 				print_list(*list);
 			}
 			else
-				tmp = tmp->prev;
+				temp = temp->prev;
 		}
 	}
 }
